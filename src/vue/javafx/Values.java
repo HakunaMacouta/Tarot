51d441a1683eb57016fbb32812853c58b36405d2
Florent Vain <Florent Vain>
2016-12-03 16:31:50 +0100

    rollbakc de vue/Values.java

--- src/vue/javafx/Values.java
+++ src/vue/javafx/Values.java
@@ -4,41 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum Values {
-	ONE ("01",1),
-	TWO ("02",2),
-	THREE ("03",3),
-	FOUR ("04",4),
-	FIVE ("05",5),
-	SIX ("06",6),
-	SEVEN ("07",7),
-	EIGHT ("08",8),
-	NINE ("09",9),
-	TEN ("10",10),
-	ELEVEN("11",11),
-	TWELVE("12",12),
-	THIRTEEN("13",13),
-	FORTEEN("14",14),
-	FIFTEEN("15",15),
-	SIXTEEN("16",16),
-	SEVENTEEN("17",17),
-	EIGHTEEN("18",18),
-	NINETEEN("19",19),
-	TWENTY("20",20),
-	TWENTYONE("21",21),
-	FOOL("The_Fool",1000),
-	JACK ("Jack",11),
-	KNIGHT ("Knight",12),
-	QUEEN ("Queen",13),
-	KING ("King",14),
-	ACE ("Ace",1);
+	ONE ("01"),
+	TWO ("02"),
+	THREE ("03"),
+	FOUR ("04"),
+	FIVE ("05"),
+	SIX ("06"),
+	SEVEN ("07"),
+	EIGHT ("08"),
+	NINE ("09"),
+	TEN ("10"),
+	ELEVEN("11"),
+	TWELVE("12"),
+	THIRTEEN("13"),
+	FORTEEN("14"),
+	FIFTEEN("15"),
+	SIXTEEN("16"),
+	SEVENTEEN("17"),
+	EIGHTEEN("18"),
+	NINETEEN("19"),
+	TWENTY("20"),
+	TWENTYONE("21"),
+	FOOL("The_Fool"),
+	JACK ("Jack"),
+	KNIGHT ("Knight"),
+	QUEEN ("Queen"),
+	KING ("King"),
+	ACE ("Ace");
 	
 	
 	private String name;
-	private int power;
 	
-	Values(String name, int power) {
+	Values(String name) {
 		this.name = name;
-		this.power = power;
 	}
 	Values(String name, String trumps) {
 		
@@ -46,10 +44,6 @@ public enum Values {
 	
 	public String toString() {
 		return this.name;
-	}
-	
-	public int getPower(){
-		return power;
 	}
 	
 	public static ArrayList<Values> getValues(boolean b) {
